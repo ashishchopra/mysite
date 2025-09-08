00b20f03c1cc505e44bc317f47822fdc4ba5b967
Ashish Chopra <ashishchopra0308@gmail.com>
2025-09-08 18:12:04 +0530

    switch from sqlite to postgres, search functionality in blog

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,15 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, TrigramSimilarity
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
-from django.core.mail import send_mail
-from django.db.models import Count
-
 from taggit.models import Tag
 
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -127,5 +128,30 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    similarity=TrigramSimilarity('title', query)
+                ).filter(similarity__gt=0.1)
+                .order_by('-similarity')
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
